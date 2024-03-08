848fa8265358c4e55ab88a209ff492085ec0933e
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-03-08 20:20:57 +0600

    Small Change

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -18,22 +18,17 @@ exports.login=async (req,res)=>{
 
     try{
         let reqBody=req.body;
-
-        let user=await UsersModel.find(reqBody)
-        res.json({status:"success",message:user})
-
+        let user= await UsersModel.find(reqBody)
         if(user.length>0){
-            //JWT Token
             let Payload={exp:Math.floor(Date.now()/1000)+(24*60*60),data:reqBody['email']}
-            let token=jwt.sign(Payload,"123-xyz");
-            res.json({status:"success",message:"User Found"})
-
-        }else{
+            let token=jwt.sign(Payload,'123-xyz-abc');
+            res.json({status:"success",message:"User Found",token:token})
+        }else {
             res.json({status:"fail",message:"No User Found"})
         }
+        res.json({status:"success",message:user})
 
-
-    }catch (e){
+    }catch (err) {
         res.json({status:"fail",message:err})
     }
 
